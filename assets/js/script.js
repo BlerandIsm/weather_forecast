99b72c31ddd5187b9f017f531bc9c6939d8975e1
Blerand Ismaili <94268979+BlerandIsm@users.noreply.github.com>
2022-02-06 10:24:18 -0500

    first event

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,21 @@ var citySearchInputEl = document.querySelector("#searched-city");
 var forecastTitle = document.querySelector("#forecast");
 var forecastContainerEl = document.querySelector("#fiveday-container");
 var pastSearchButtonEl = document.querySelector("#past-search-buttons");
+
+var formSubmitHandler =  function(event){
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+    if(city){
+        getCityWeather(city);
+        get5Day(city);
+        cities.unshift({city});
+        cityInputEl.value="";s
+    } else{
+        alert("Please enter a valid city");
+    }
+    saveSearch();
+    pastSearchButtonEl(city);
+}
+var saveSearch = function(){
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
